075647542be09500c22d5c1dc41fd83036b82fe2
Reseda <pro@MacBook-Pro-PRO.local>
2023-03-02 19:44:28 +0400

    Add homework_1

--- homework_1/Main.java
+++ homework_1/Main.java
@@ -16,18 +16,19 @@ public class Main {
         List<Relationship> relationships = tree.getRelationships();
         
         // Добавление людей
-        Person person1 = new Person("Резеда", LocalDate.of(1950, 5, 10), null);
-        Person person2 = new Person("Максим", LocalDate.of(1955, 3, 15), null);
-        Person person3 = new Person("Андрей", LocalDate.of(1950, 5, 10), null);
-        Person person4 = new Person("Камиль", LocalDate.of(1950, 5, 10), null);
-        Person person5 = new Person("Венера", LocalDate.of(1950, 5, 10), null);
-        
+        Person person1 = new Person("Регина", LocalDate.of(1950, 5, 30), null);
+        Person person2 = new Person("Максим", LocalDate.of(1991, 7, 03), null);
+        Person person3 = new Person("Александр", LocalDate.of(2010, 8, 17), null);
+        Person person4 = new Person("Николай", LocalDate.of(1959, 1, 06), null);
+        Person person5 = new Person("Валентина", LocalDate.of(1955, 2, 11), null);
+        Person person6 = new Person("Раиса", LocalDate.of(1992, 1, 11), null);
+
         tree.addPerson(person1);
         tree.addPerson(person2);
         tree.addPerson(person3);
         tree.addPerson(person4);
         tree.addPerson(person5);
-
+        tree.addPerson(person6);
         
         // Добавление отношений
         Relationship relationship1 = new Relationship(person1, person2, RelationshipType.SPOUSE);
@@ -36,6 +37,37 @@ public class Main {
         tree.addRelationship(relationship2);
         Relationship relationship3 = new Relationship(person1, person4, RelationshipType.CHILD);
         tree.addRelationship(relationship3);
+        Relationship relationship4 = new Relationship(person1, person5, RelationshipType.CHILD);
+        tree.addRelationship(relationship4);
+        Relationship relationship5 = new Relationship(person2, person1, RelationshipType.SPOUSE);
+        tree.addRelationship(relationship5);
+        Relationship relationship6 = new Relationship(person2, person3, RelationshipType.PARENT);
+        tree.addRelationship(relationship6);
+        Relationship relationship7 = new Relationship(person3, person1, RelationshipType.PARENT);
+        tree.addRelationship(relationship7);
+        Relationship relationship8 = new Relationship(person3, person2, RelationshipType.PARENT);
+        tree.addRelationship(relationship8);
+        Relationship relationship9 = new Relationship(person4, person1, RelationshipType.PARENT);
+        tree.addRelationship(relationship9);
+        Relationship relationship10 = new Relationship(person4, person5, RelationshipType.SPOUSE);
+        tree.addRelationship(relationship10);
+        Relationship relationship11 = new Relationship(person5, person4, RelationshipType.SPOUSE);
+        tree.addRelationship(relationship11);
+        Relationship relationship12 = new Relationship(person5, person1, RelationshipType.PARENT);
+        tree.addRelationship(relationship12);
+        Relationship relationship13 = new Relationship(person6, person4, RelationshipType.CHILD);
+        tree.addRelationship(relationship13);
+        Relationship relationship14 = new Relationship(person6, person5, RelationshipType.CHILD);
+        tree.addRelationship(relationship14);
+        Relationship relationship15 = new Relationship(person4, person6, RelationshipType.PARENT);
+        tree.addRelationship(relationship15);
+        Relationship relationship16 = new Relationship(person5, person6, RelationshipType.PARENT);
+        tree.addRelationship(relationship16);
+        Relationship relationship17 = new Relationship(person1, person6, RelationshipType.SIBLING);
+        tree.addRelationship(relationship17);
+
+
+
 
         // // Вывод информации о каждом человеке
         // System.out.println("People:");
